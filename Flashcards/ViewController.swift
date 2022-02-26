123f716ca65b38d554fc81a79d2da340e73a1684
Russell Elliott <russell@quadsquad.net>
2022-02-26 11:34:52 -0800

    Base functionality compelte. User can see answer to flash card upon tapping it.

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Label for the answer
+    @IBOutlet weak var backLabel: UILabel!
+    
+    //Label for the question
+    @IBOutlet weak var frontLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    //Function that shows the answer when the user taps on the flashcard
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        //set the question label to be hidden so the answer is visible
+        frontLabel.isHidden = true;
+    }
+    
 }
 
