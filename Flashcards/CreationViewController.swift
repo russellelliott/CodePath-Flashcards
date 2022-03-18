0d75fb32bd411d39c0e47c2f7dfe2355cce58d4c
Russell Elliott <russell@quadsquad.net>
2022-03-18 12:12:59 -0700

    User is able to click the 'Add New Card' button and specify the question and answer for said card.

--- Flashcards/CreationViewController.swift
+++ Flashcards/CreationViewController.swift
@@ -11,6 +11,9 @@ class CreationViewController: UIViewController {
     
     var flashcardsController: ViewController!
     
+    @IBOutlet weak var questionTextField: UITextField!
+    @IBOutlet weak var answerTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,7 +25,24 @@ class CreationViewController: UIViewController {
     }
     
     @IBAction func done(_ sender: Any) {
-        flashcardsController.updateFlashcard(question: "question", answer: "answer")
+        //Get the text from the quesiton text field
+        let questionText = questionTextField.text
+        
+        //Get the text from the answer text field
+        let answerText = answerTextField.text
+        
+        //Check if the user has entered a question and an answer on the card. If not, display an error.
+        if(questionText=="" || answerText==""){
+            //If any of the fields are empty, display error
+            // create the alert
+            let alert = UIAlertController(title: "Error: Insufficent Data", message: "Your flash card doesn't include text for the question and/or the answer. Make sure you enter a question and answer for your flashcard.", preferredStyle: UIAlertController.Style.alert)
+            // add an action (button)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            // show the alert
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+        flashcardsController.updateFlashcard(question: questionText!, answer: answerText!)
         dismiss(animated: true)
     }
     
