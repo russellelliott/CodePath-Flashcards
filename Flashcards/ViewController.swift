2d9e8eb0e6079e55ff6ae200049b17bc19176618
Russell Elliott <russell@quadsquad.net>
2022-03-19 11:37:47 -0700

    updateFlashcard() function works. Added buttons which will go to the previous card and the next card.

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -21,9 +21,18 @@ class ViewController: UIViewController {
     //Label for the question
     @IBOutlet weak var frontLabel: UILabel!
     
+    //Array to store the flashcards
+    var flashcards = [Flashcard]()
+    
+    //Current flashcard index
+    var currentIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Add the first flashcard
+        updateFlashcard(question: "Sample Question", answer: "Sample Answer")
     }
 
     //Function that shows the answer when the user taps on the flashcard
@@ -47,6 +56,20 @@ class ViewController: UIViewController {
         let flashcard = Flashcard(question: question, answer: answer)
         frontLabel.text = flashcard.question;
         backLabel.text = flashcard.answer;
+        //Append flashcard to the list of flashcards
+        flashcards.append(flashcard)
+        //Debug statements to ensure the code is working
+        print("Added new flashcard")
+        print("There are now \(flashcards.count) flashcards")
+    }
+    
+    //User tapped the "prev" button to go back to the previous card
+    @IBAction func prev(_ sender: Any) {
+    }
+    
+    
+    //User tapped the "next" button to go to the next card
+    @IBAction func next(_ sender: Any) {
     }
     
 }
